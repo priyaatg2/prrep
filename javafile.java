d001bdabc45b9a56a8f1fc53d2482b748ba8edb7
priyaatg2 <priyahalwaidinesh@gmail.com>
2025-02-11 14:37:58 +0530

    Update javafile.java

--- javafile.java
+++ javafile.java
@@ -1,6 +1,7 @@
 class javafile{  
     public static void main(String args[]){  
         
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java"); 
+    System.out.println("2nd line"); 
     }  
 }  
